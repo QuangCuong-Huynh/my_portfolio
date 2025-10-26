ec4c6fa71914239db8cc0126f07d27637fa281e4
QuangCuong-Huynh <quangcuong.hu@gmail.com>
2025-10-26 12:15:16 +0700

    Add ESLint configuration for React project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+// my_portfolio/.eslintrc.js
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+};
